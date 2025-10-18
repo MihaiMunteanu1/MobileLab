219b0051e96e5118da7cdd22931773426b6b8c9d
Mihai Munteanu <mihai.munteanu@stud.ubbcluj.ro>
2025-10-18 16:10:34 +0300

    ionic1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -19,34 +18,27 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
 /* Theme variables */
 import './theme/variables.css';
+import './theme/styles.css';
+
+import { ItemEdit, ItemList } from './todo';
+import { ItemProvider } from './todo/ItemProvider';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <ItemProvider>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/items" component={ItemList} exact={true}/>
+          <Route path="/item" component={ItemEdit} exact={true}/>
+          <Route path="/item/:id" component={ItemEdit} exact={true}/>
+          <Route exact path="/" render={() => <Redirect to="/items"/>}/>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </ItemProvider>
   </IonApp>
 );
 
