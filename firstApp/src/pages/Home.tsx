2fc47ba79e8a797d2fafcf3553bb78938288c20c
Mihai Munteanu <mihai.munteanu@stud.ubbcluj.ro>
2025-10-26 11:53:14 +0200

    Ionic2-network status, pagination, search, filter

---
+++ firstApp/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import {IonContent, IonHeader, IonLabel, IonList, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+import { useAppState } from './useAppState';
+import { useNetwork } from './useNetwork';
+import { usePreferences } from './usePreferences';
+
+const Home: React.FC = () => {
+  const { appState } = useAppState();
+  const { networkStatus } = useNetwork();
+  usePreferences();
+  return (
+      <IonLabel>App state is {JSON.stringify(appState)} <br/> Network status
+          is {JSON.stringify(networkStatus)}</IonLabel>
+  );
+};
+
+export default Home;
